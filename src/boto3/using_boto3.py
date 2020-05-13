6e995adca98508bcec209cd1970db1aaca0dbeca
imaxyz <YI201610@users.noreply.github.com>
2020-05-14 07:48:59 +0900

    boto3でs3に画像をアップロード

---
+++ src/boto3/using_boto3.py
@@ -0,0 +1,15 @@
+import boto3
+
+# s3に接続して、バケット名を取得する
+s3 = boto3.resource('s3')
+for bucket in s3.buckets.all():
+    print('s3 bucket name: ', bucket.name)
+
+# Upload a new file
+filename = 'IMG_0048.jpg'
+data = open(filename, 'rb')
+result = s3.Bucket('pybasic-s3').put_object(Key=filename, Body=data)
+
+print('result: ', result)
+
+
