bf83dca31b9ed1b16168ced10c3666a7f9215104
imaxyz <YI201610@users.noreply.github.com>
2021-07-12 07:03:30 +0900

    wip

--- src/building-deep-learning/chapter3/sample3_6_2.py
+++ src/building-deep-learning/chapter3/sample3_6_2.py
@@ -1,3 +1,4 @@
+import pickle
 import sys, os
 import numpy as np
 from mnist import load_mnist    # mnistライブラリのload_mnist関数をインポート
@@ -16,40 +17,93 @@ def get_normalized_test_data():
     return test_images, test_labels
 
 
-def init_network():
+def get_sample_weight():
+    """サンプルの重みを返す"""
 
-    pass
+    # sample_weight.pkl ... 重みWとバイアスbのパラメータがディクショナリ型で登録されている
+    with open('sample_weight.pkl') as f:
+        network = pickle.load(f)
+
+    return network
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def stable_sigmoid(x):
+    """
+    安定したシグモイド関数
+    https://www.delftstack.com/ja/howto/python/sigmoid-function-python/
+
+    :param x:
+    :return:
+    """
+
+    sig = np.where(x < 0, np.exp(x) / (1 + np.exp(x)), 1 / (1 + np.exp(-x)))
+    return sig
+
+
+def softmax(a):
+    c = np.max(a)
+
+    # オーバーフロー対策をしながら指数関数を計算する
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+def predict(network, x):
+    """
+    3層ニューラルネットワークで予測する
+
+    設計:
+        - 入力層: 784
+        - 第1隱れ層: 50
+        - 第2隱れ層: 100
+        - 出力層: 10
+
+    :param network:
+        ネットワーク情報（重み、バイアス）
+    :param x:
+        入力
+    :return:
+    """
+
+    # 各層の重みを取得
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = stable_sigmoid(a1)
+
+    a2 = np.dot(z1, W2) + b2
+    z2 = stable_sigmoid(a2)
+
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
 
 
 def main():
 
     sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 
-    # (訓練画像、訓練ラベル), (テスト画像、テストラベル)の形式でmnistデータを取得
-    (train_images, train_labels), (test_images, test_labels) = load_mnist(
-        # 画像を一次元配列にするか
-        flatten=True,
+    test_images, test_labels = get_normalized_test_data()
 
-        # 画像のピクセル値を0.0~1.0に正規化するか
-        normalize=False,
+    sample_weight = get_sample_weight()
 
-        # one-hot表現を行うか
-        one_hot_label=False,
-    )
+    accuracy_cnt = 0
+    for i in range(len(test_images)):
+        y = predict(sample_weight, test_images[i])
+        p = np.argmax(y)  # 最も確率の高い要素のインデックスを取得
+        if p == test_labels[i]:
+            accuracy_cnt += 1
 
-    # 訓練データの最初の要素を取得
-    train_image = train_images[0]
-    train_label = train_labels[0]
-
-    print('train_image.shape: ', train_image.shape)  # (784,)
-    print('train_label: ', train_label)  # 5
-
-    # 形状を元の画像サイズに変形
-    # MNISTの画像データのサイズは、28x28
-    train_image = train_image.reshape(28, 28)
-    # img = img.reshape(50, 50)  # 形状を元の画像サイズに変形
-    print('train_image reshaped: ', train_image.shape)  # (28, 28)
-
+    print("Accuracy:" + str(float(accuracy_cnt) / len(test_images)))
 
 
 if __name__ == '__main__':
