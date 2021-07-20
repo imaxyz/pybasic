b4eed04b8d5d07b366e6948ab8e9208be9aa6157
imaxyz <YI201610@users.noreply.github.com>
2021-07-21 06:32:45 +0900

    二乗和誤差の実装を追加

---
+++ src/building-deep-learning/chapter4/sample4_2_1.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def sum_squared_error(y: np.ndarray, t: np.ndarray):
+    """２乗和誤差を返す"""
+    return 0.5 * np.sum((y-t)**2)
+
+
+def main():
+
+    # 2を正解とする
+    t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+    # 2の確率を最も高くする
+    y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0, 0.1, 0, 0])
+    y1 = sum_squared_error(y, t)
+
+    # 7の確率を最も高くする
+    y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0, 0.6, 0, 0])
+    y2 = sum_squared_error(y, t)
+    pass
+
+
+if __name__ == '__main__':
+    main()
